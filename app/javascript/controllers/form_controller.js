69bc6d4ea40ce5b4dfb8410c781d520283d20aee
Samuel Tettey <busy@seenbysam.com>
2025-06-13 11:13:22 +0200

    improved 'go back' logic to make elements re-clickable

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -7,66 +7,73 @@ export default class extends Controller {
   }
 
   goToStep(event) {
-    const targetStepIndex = parseInt(event.target.dataset.step, 10);
-    const selectedValue = event.target.value;
-    const inputName = event.target.name;
-    const selectedFood = document.querySelector('input[name="food_type_selection"]:checked')?.value;
+  const targetStepIndex = parseInt(event.target.dataset.step, 10);
+  const selectedValue = event.target.value;
+  const inputName = event.target.name;
+  const selectedFood = document.querySelector('input[name="food_type_selection"]:checked')?.value;
 
-    if (selectedFood && this.hasFoodOutputTarget) {
-      this.foodOutputTarget.innerHTML = `Alright, we're gonna cook a ${selectedFood} dish.<br>How difficult should the preperation be?`;
-    }
+  const currentStepIndex = this.stepTargets.findIndex(el => !el.classList.contains("d-none"));
 
-     if (inputName === "food_type_selection" && selectedValue === "Shuffle") {
-      const foodOptions = ["Meat", "Vegan", "Vegetarian", "Seafood"];
-      const randomFood = foodOptions[Math.floor(Math.random() * foodOptions.length)];
-      const foodInput = document.querySelector(`input[name="food_type_selection"][value="${randomFood}"]`);
-      if (foodInput) foodInput.checked = true;
-
-      alert("Thank you for trusting us! We'll surprise you with a " + randomFood + " dish!");
-
-      // ✅ Show surprise food in shuffle output
-      if (this.hasShuffleOutputTarget) {
-        this.shuffleOutputTarget.innerText = `🎲 Your surprise food type is: ${randomFood}`;
-        this.shuffleOutputTarget.classList.remove("d-none");
-      }
-
-      // ✅ ALSO update the foodOutput header text
-      if (this.hasFoodOutputTarget) {
-        this.foodOutputTarget.innerHTML = `Alright, we're gonna cook a ${randomFood} dish.<br>How difficult should the preperation be?`;
-      }
-    }
-
-    else if (inputName === "music_genres[]") {
-      if (selectedValue === "Shuffle" && event.target.checked) {
-        document.querySelectorAll('input[name="music_genres[]"]').forEach(input => {
-          input.checked = false;
-        });
-
-        const genreOptions = ["Pop", "Rock", "Hip-Hop", "Rap", "R&B", "Indie", "Electronic", "Dance", "Alternative", "Jazz", "Classical", "Folk", "Country", "Metal", "Punk", "Blues", "Reggae", "Soul", "Funk", "Techno", "Afro"];
-        const randomGenre = genreOptions[Math.floor(Math.random() * genreOptions.length)];
-        const genreInput = document.querySelector(`input[name="music_genres[]"][value="${randomGenre}"]`);
-        if (genreInput) genreInput.checked = true;
-
-        alert(`Surprise! We picked '${randomGenre}' for your music genre.`);
-
-        if (this.hasGenreShuffleOutputTarget) {
-          this.genreShuffleOutputTarget.innerText = `🎧 Your surprise genre is: ${randomGenre}`;
-          this.genreShuffleOutputTarget.classList.remove("d-none");
-        }
-
-        event.target.checked = false;
-      } else {
-        if (this.hasGenreShuffleOutputTarget) {
-          this.genreShuffleOutputTarget.classList.add("d-none");
-          this.genreShuffleOutputTarget.innerText = "";
-        }
-      }
-
-      this.toggleMusicFormatDisplay();
-    }
-
-    this.showStep(targetStepIndex);
+  // Hide music format when going back
+  if (this.hasMusicFormatTarget && targetStepIndex < currentStepIndex) {
+    this.musicFormatTarget.classList.add("d-none");
   }
+
+  // Update food description if selected
+  if (selectedFood && this.hasFoodOutputTarget) {
+    this.foodOutputTarget.innerHTML = `Alright, we're gonna cook a ${selectedFood} dish.<br>How difficult should the preparation be?`;
+  }
+
+  if (inputName === "food_type_selection" && selectedValue === "Shuffle") {
+    const foodOptions = ["Meat", "Vegan", "Vegetarian", "Seafood"];
+    const randomFood = foodOptions[Math.floor(Math.random() * foodOptions.length)];
+    const foodInput = document.querySelector(`input[name="food_type_selection"][value="${randomFood}"]`);
+    if (foodInput) foodInput.checked = true;
+
+    alert("Thank you for trusting us! We'll surprise you with a " + randomFood + " dish!");
+
+    if (this.hasShuffleOutputTarget) {
+      this.shuffleOutputTarget.innerText = `🎲 Your surprise food type is: ${randomFood}`;
+      this.shuffleOutputTarget.classList.remove("d-none");
+    }
+
+    if (this.hasFoodOutputTarget) {
+      this.foodOutputTarget.innerHTML = `Alright, we're gonna cook a ${randomFood} dish.<br>How difficult should the preparation be?`;
+    }
+  }
+
+  else if (inputName === "music_genres[]") {
+    if (selectedValue === "Shuffle" && event.target.checked) {
+      document.querySelectorAll('input[name="music_genres[]"]').forEach(input => {
+        input.checked = false;
+      });
+
+      const genreOptions = ["Pop", "Rock", "Hip-Hop", "Rap", "R&B", "Indie", "Electronic", "Dance", "Alternative", "Jazz", "Classical", "Folk", "Country", "Metal", "Punk", "Blues", "Reggae", "Soul", "Funk", "Techno", "Afro"];
+      const randomGenre = genreOptions[Math.floor(Math.random() * genreOptions.length)];
+      const genreInput = document.querySelector(`input[name="music_genres[]"][value="${randomGenre}"]`);
+      if (genreInput) genreInput.checked = true;
+
+      alert(`Surprise! We picked '${randomGenre}' for your music genre.`);
+
+      if (this.hasGenreShuffleOutputTarget) {
+        this.genreShuffleOutputTarget.innerText = `🎧 Your surprise genre is: ${randomGenre}`;
+        this.genreShuffleOutputTarget.classList.remove("d-none");
+      }
+
+      event.target.checked = false;
+    } else {
+      if (this.hasGenreShuffleOutputTarget) {
+        this.genreShuffleOutputTarget.classList.add("d-none");
+        this.genreShuffleOutputTarget.innerText = "";
+      }
+    }
+
+    this.toggleMusicFormatDisplay();
+  }
+
+  this.showStep(targetStepIndex);
+}
+
 
   submit() {
   this.element.querySelector("form").submit();
