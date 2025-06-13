65624a4e579f37c29ff76ad574fe9d7102ca2fb1
marta borges <marta_borges_08@hotmail.com>
2025-06-13 12:28:40 +0200

    recipes IV

--- app/javascript/controllers/music_suggestions.js
+++ app/javascript/controllers/music_suggestions.js
@@ -32,14 +32,4 @@ export default class extends Controller {
     this.selectedCardId = event.currentTarget.dataset.id
     this.buttonTarget.classList.remove("disabled")
   }
-  submitForm(event) {
-    event.preventDefault(); // Prevent default form submission
-    if (this.selectedCardId) {
-      // Optionally, you can do something with this.selectedCardId
-      // For example, you can send it to the server or use it in the next step
-      this.element.submit(); // Submit the form programmatically
-    } else {
-      alert("Please select a music suggestion before proceeding.");
-    }
-  }
 }
