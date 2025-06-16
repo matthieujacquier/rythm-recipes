caf99776f58a19604d7bb94d7444b34091e006a6
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-16 11:26:18 +0200

    ./ is required

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,9 @@
 
 pin "application", preload: true
 pin "controllers", to: "controllers.js"
+pin "controllers/recipe_suggestions_controller", to: "controllers/recipe_suggestions_controller.js"
+pin "controllers/music_suggestions_controller", to: "controllers/music_suggestions_controller.js"
+pin "controllers/form_controller", to: "controllers/form_controller.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
