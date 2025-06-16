6706b384589576c9f10f0f3758782b415e504efa
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-16 10:54:46 +0200

    application.js update

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,10 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "controllers/application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
 import RecipeSuggestionsController from "./recipe_suggestions_controller";
 application.register("recipe-suggestions", RecipeSuggestionsController);
 import MusicSuggestionsController from "./music_suggestions_controller";
 application.register("music-suggestions", MusicSuggestionsController);
-import FormController from "./form_controller"
-application.register("form", FormController)
+import FormController from "./form_controller";
+application.register("form", FormController);
