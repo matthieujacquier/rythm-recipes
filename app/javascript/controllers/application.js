5ac94bc9af5ea13743bac59adbfbda950beb09c5
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-16 11:13:36 +0200

    more heroku

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 import "controllers"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+eagerLoadControllersFrom("controllers", application)
 
 export { application }
