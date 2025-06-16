d93871f1d31a73012437a7c19b24803800807393
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-16 10:49:02 +0200

    updatingControllerName

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,9 +2,9 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import RecipeSuggestionsController from "./recipe_suggestions";
+import RecipeSuggestionsController from "./recipe_suggestions_controller";
 application.register("recipe-suggestions", RecipeSuggestionsController);
-import MusicSuggestionsController from "./music_suggestions"
+import MusicSuggestionsController from "./music_suggestions_controller"
 application.register("music-suggestions", MusicSuggestionsController);
 import FormController from "./form_controller"
 application.register("form", FormController)
