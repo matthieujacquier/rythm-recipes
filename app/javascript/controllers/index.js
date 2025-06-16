2170877e7429593f1139f8c75be2f743bdb60d86
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-16 11:41:13 +0200

    removing individual loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,12 +2,3 @@
 import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
-import RecipeSuggestionsController from "./recipe_suggestions_controller";
-application.register("recipe-suggestions", RecipeSuggestionsController);
-import MusicSuggestionsController from "./music_suggestions_controller";
-application.register("music-suggestions", MusicSuggestionsController);
-import FormController from "./form_controller";
-application.register("form", FormController);
-
-// adding .js in the filename does not work
-// removing ./ in the path does not work and breaks localhost.
