05d2bc92395ed5882a3b095eac08401273175b5f
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-06-18 17:15:32 +0200

    again

--- app/services/save_recipes.rb
+++ app/services/save_recipes.rb
@@ -1,13 +1,14 @@
 require_relative '../../config/environment'
 require 'json'
 
-Class SaveRecipes
-recipes = Recipe.all.as_json(except: [:id, :created_at, :updated_at])
+class SaveRecipes
+  def self.call
+    recipes = Recipe.all.as_json(except: [:id, :created_at, :updated_at])
 
-File.open("recipes.json", "w") do |f|
-  f.write(JSON.pretty_generate(recipes))
-end
+    File.open("recipes.json", "w") do |f|
+      f.write(JSON.pretty_generate(recipes))
+    end
 
-puts "Recipes saved to recipes.json"
-
+    puts "Recipes saved to recipes.json"
+  end
 end
